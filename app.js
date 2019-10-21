7a87eca75bfba8f0d32ed7754c1eddb81e25b538
Sjoerd Wolters <sjoerd@woltersonline.net>
2019-10-21 11:50:33 +0200

    Deleted additional app.js + moved content to right app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-class App {
-    import * as firebase from "firebase/app";
-
-    // Add the Firebase services that you want to use
-    import "firebase/firestore";
-
-}
---
+++ src/app.js
@@ -0,0 +1,9 @@
+"use strict"
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/firestore";
+
+class App {
+
+}
