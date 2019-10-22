ef93687ad0bdd0072b23d4b33956a99aee3f19d0
Sjoerd Wolters <sjoerd@woltersonline.net>
2019-10-22 14:23:00 +0200

    removed all db-related things of html + added database.js

---
+++ src/database.js
@@ -0,0 +1,39 @@
+"use strict";
+
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB_duHYIB9Xj3jXhShsI8cfMxDXzEzePZA",
+    authDomain: "spotyourmusic-4b53a.firebaseapp.com",
+    databaseURL: "https://spotyourmusic-4b53a.firebaseio.com",
+    projectId: "spotyourmusic-4b53a",
+    storageBucket: "spotyourmusic-4b53a.appspot.com",
+    messagingSenderId: "694274207492",
+    appId: "1:694274207492:web:cf9e0fcbcd61b616de6a51"
+};
+
+let db = "";
+
+class DB{
+    constructor(){
+        firebase.initializeApp(firebaseConfig);
+        db = firebase.firestore();
+    }
+
+    addSong(song){
+        return db.collection("Songs").add(song);
+    }
+
+    getAllSongs(){
+        return db.collection("Songs").get();
+    }
+
+    getSong(){
+        return db.collection("Songs").doc(id).get();
+    }
+
+    findPossibleSongs(keyword){
+        return true;
+    }
+}
