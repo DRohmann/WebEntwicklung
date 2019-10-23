8286e73909d012dc5e618f6a126760b3b198388a
Sjoerd Wolters <sjoerd@woltersonline.net>
2019-10-23 10:55:31 +0200

    added database-read on title-page.js started title-page.css fitting changes to index.html, database.js and app.js

--- src/section_titlepage/title-page.js
+++ src/section_titlepage/title-page.js
@@ -16,12 +16,11 @@ class TitlePage {
       // Anzuzeigende HTML-Elemente ermitteln
       let section = document.querySelector("#section_titlepage").cloneNode(true);
 
-      /*_db.getAllReceipes().then(function(querySnapshot)
+      _db.getSong("gRkICzgt9paeGXJ54aPS").then(function(doc)
       {
-        console.log("Receipes loaded");
-        onFinishedLoading(querySnapshot);
-      });*/
-      //Rauskommentiert da unnötig für uns, eher für Suchen- oder Startseite
+        console.log("Song loaded");
+        onFinishedLoading(doc);
+      });
 
       return {
           className: "section_titlepage",
@@ -43,30 +42,14 @@ class TitlePage {
     }
   }
 
-  let onFinishedLoading = (receipes) =>
+  let onFinishedLoading = (doc) =>
     {
-      /*receipes.forEach(function(doc) {
-        let list = document.getElementById("receipe-list");
-        let table = document.getElementById("receipe-table");
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data().name);
-
-        let receipe= document.getElementById("dummy").cloneNode(true);
-        receipe.setAttribute("id", "receipe_" + doc.id);
-        table.appendChild(receipe);
-
-        document.querySelectorAll("#receipe_" + doc.id + " > .name")[0].textContent = doc.data().name;
-        let buttons = document.querySelectorAll("#receipe_" + doc.id +" > .links a");
-        buttons[0].setAttribute("href", "/receipe/show/" + doc.id);
-        buttons[1].setAttribute("href", "/receipe/edit/" + doc.id);
-        buttons[2].setAttribute("href", "/receipe/delete/" + doc.id);
-
-        });*/
-        /*Rauskommentiert da unnötig für uns, hier würde ja dann eher
-        sowas rein kommen */
-
-        _app._router.updatePageLinks();
-        console.log("Alle Page Links updated");
+        console.log("Finished loading");
+        document.getElementById("title_cell_title").textContent = doc.data().TITLE;
+        document.getElementById("title_cell_artist").textContent = doc.data().ARTIST;
+        document.getElementById("title_cell_genre").textContent = doc.data().GENRE;
+        document.getElementById("title_cell_album").textContent = doc.data().ALBUM;
+        document.getElementById("title_cell_lyrics").textContent = doc.data().SONGTEXT;
     }
 
 
